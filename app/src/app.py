4ff1dd5e41ebf11bd0a0390edc112e3b1b7a40b8
AnnaKu <annaku@radware.com>
2020-12-07 19:27:17 +0200

    Add /hello, /login

--- app/src/app.py
+++ app/src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -11,6 +12,29 @@ def hello_world():
 @app.route('/api')
 def rest_hello_world():
     return '{"id":1,"message":"Flask: Hello World from Docker"}'
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+
+        if request.form['username'] == "ofir":
+            return render_template('hello.html')
+        # if valid_login(request.form['username'],
+        #                request.form['password']):
+        #     return log_the_user_in(request.form['username'])
+        # else:
+        #     error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
