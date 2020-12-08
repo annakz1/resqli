68ef51fda9823034efa9886c17c9063675cc1664
DanielGilon <danielg@radware.com>
2020-12-08 15:03:24 +0200

    Run o9n all fields

--- app/src/app.py
+++ app/src/app.py
@@ -10,10 +10,22 @@ model = predict.Model()
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        product_name = request.form['product']
-        prediction = model.predict_our_model(product_name)
+        predictions = []
 
-        return render_template('index.html', prediction=prediction, product_name=product_name)
+        for key in ['product', 'address']:
+
+            field_val = request.form[key]
+
+            prediction = model.predict_our_model(field_val)
+
+            pred_obj = {
+                "field": key,
+                "field_val": field_val,
+                "prediction": prediction,
+            }
+            predictions.append(pred_obj)
+
+        return render_template('index.html', predictions=predictions)
 
     return render_template('index.html')
 
