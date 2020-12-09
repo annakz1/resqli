c66981d6e0e648cf2d42e12d0d7a6e890accce4c
AnnaKu <annaku@radware.com>
2020-12-09 11:42:54 +0200

    adding ajax and jquery- not fully working

--- app/src/app.py
+++ app/src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, jsonify
 from flask import request
 import predict
 
@@ -9,25 +11,50 @@ model = predict.Model()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    #     predictions = []
+    #
+    #     for key in ['product', 'address']:
+    #         field_val = request.form[key]
+    #
+    #         prediction = "true"  # model.predict_our_model(field_val)
+    #
+    #         pred_obj = {
+    #             "field": key,
+    #             "field_val": field_val,
+    #             "prediction": prediction,
+    #         }
+    #         predictions.append(pred_obj)
+    #
+    #     return render_template('index.html', predictions=predictions)
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+@app.route('/signUpUser', methods=['POST'])
+def signUpUser():
+    address = request.form['address']
+    product = request.form['product']
+
     if request.method == 'POST':
         predictions = []
 
-        for key in ['product', 'address']:
-
-            field_val = request.form[key]
+    for key in ['product', 'address']:
+        field_val = request.form[key]
 
             prediction = model.predict_our_model(field_val)
 
-            pred_obj = {
-                "field": key,
-                "field_val": field_val,
-                "prediction": prediction,
-            }
-            predictions.append(pred_obj)
+        pred_obj = {
+            "field": key,
+            "field_val": field_val,
+            "prediction": prediction,
+        }
+        predictions.append(pred_obj)
 
-        return render_template('index.html', predictions=predictions)
+    # return render_template('index.html', predictions=predictions)
 
-    return render_template('index.html')
+    # return json.dumps(predictions)
+    # print(render_template('dynamic_data.html'))
+    return jsonify(predictions, render_template('dynamic_data.html', predictions=predictions))
 
 
 if __name__ == '__main__':
