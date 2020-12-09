3145d61d431413428d6848afbf985deaf8a7ae6f
AnnaKu <annaku@radware.com>
2020-12-09 13:35:41 +0200

    complete ajax - interactive response

--- app/src/app.py
+++ app/src/app.py
@@ -1,5 +1,4 @@
 import json
-
 from flask import Flask, render_template, jsonify
 from flask import request
 import predict
@@ -11,37 +10,19 @@ model = predict.Model()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    #     predictions = []
-    #
-    #     for key in ['product', 'address']:
-    #         field_val = request.form[key]
-    #
-    #         prediction = "true"  # model.predict_our_model(field_val)
-    #
-    #         pred_obj = {
-    #             "field": key,
-    #             "field_val": field_val,
-    #             "prediction": prediction,
-    #         }
-    #         predictions.append(pred_obj)
-    #
-    #     return render_template('index.html', predictions=predictions)
     if request.method == 'GET':
         return render_template('index.html')
 
 
 @app.route('/signUpUser', methods=['POST'])
 def signUpUser():
-    address = request.form['address']
-    product = request.form['product']
-
     if request.method == 'POST':
         predictions = []
 
     for key in ['product', 'address']:
         field_val = request.form[key]
 
-            prediction = model.predict_our_model(field_val)
+        prediction = model.predict_our_model(field_val)
 
         pred_obj = {
             "field": key,
@@ -50,11 +31,7 @@ def signUpUser():
         }
         predictions.append(pred_obj)
 
-    # return render_template('index.html', predictions=predictions)
-
-    # return json.dumps(predictions)
-    # print(render_template('dynamic_data.html'))
-    return jsonify(predictions, render_template('dynamic_data.html', predictions=predictions))
+    return jsonify(render_template('dynamic_data.html', predictions=predictions))
 
 
 if __name__ == '__main__':
--- app/src/static/js/signUp.js
+++ app/src/static/js/signUp.js
@@ -5,58 +5,10 @@ $(function(){
             data: $('form').serialize(),
             type: 'POST',
             success: function(response){
-                // alert(response)
-                // $("html").append(response);
-                // $('form').append(response);
-                // document.getElementById('anna').innerHTML = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-                $("#result").text(response);
-                // var response_obj = JSON.parse(response);
-
-                // alert(response);
-                //
-                // var fatherDiv = document.getElementById('result');
-                //
-                // for (var i = 0; i < response_obj.length; i++) {
-                //     var obj = response_obj[i];
-                //
-                //     var div = document.createElement('div');
-                //
-                //     var div1 = document.createElement('div');
-                //     div1.innerHTML = obj.field  + ": ";
-                //     div.appendChild(div1);
-                //
-                //     var div2 = document.createElement('div');
-                //     div2.innerHTML = obj.field_val;
-                //     div.appendChild(div2);
-                //
-                //     var div3 = document.createElement('div');
-                //     div3.innerHTML = "Our prediction: ";
-                //     div.appendChild(div3);
-                //
-                //     var div4 = document.createElement('div');
-                //     div4.innerHTML = obj.prediction;
-                //     div.appendChild(div4);
-                //
-                //     fatherDiv.appendChild(div);
-                // }
-
-
-                // // we create the <div> element:
-                // var div = document.createElement('div');
-                // // give it the id that we're looking for:
-                // div.id = 'ajaxResponse';
-                // // append it to the document.body:
-                // document.body.appendChild(div);
-                // // find the element we want to work with (the <div> we
-                // // created, above:
-                // var responseDiv = document.getElementById('result');
-                // set its innerHTML:
-                // responseDiv.innerHTML = "This is a div.";
+                document.getElementById('result').innerHTML = response
             },
             error: function(error){
-                // $("#result").text(error);
-                // alert(error)
-                // console.log(error);
+                $("#result").text(error);
             }
         });
     });
